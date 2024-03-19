fa362b91398f3e2a8860b69eb6984717cc2262cc
MatNepo <matnepo@gmail.com>
2024-03-19 15:20:59 +0300

    smth changed

---
+++ labs/lab8  - lab_MS_SV5/lab_MS_SV5_extra_task/slave_ODD.sv
@@ -0,0 +1,12 @@
+`timescale 1 ns / 1 ns
+module slave_ODD (
+    bus_MM.slave bus,
+    output bit[7:0] D_odd
+);
+    always_ff @(posedge bus.CLK)
+        if (bus.reset)
+            D_odd <= 0;
+        else
+            if ((bus.address ==? 8'b???????1) & (bus.write == '1))
+                D_odd <= bus.writedata;
+endmodule
