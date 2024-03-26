4c6a48355a55462a4bbea84e359a1b10a75279d1
MatNepo <matnepo@gmail.com>
2024-03-26 16:08:55 +0300

    file removed

--- labs/lab9 - lab_PD2/.files/lab_PD3_extra_task/SP_unit/SP_unit_inst.v
+++
@@ -1,6 +0,0 @@
-	SP_unit u0 (
-		.source     (<connected-to-source>),     //    sources.source
-		.probe      (<connected-to-probe>),      //     probes.probe
-		.source_clk (<connected-to-source_clk>)  // source_clk.clk
-	);
-
