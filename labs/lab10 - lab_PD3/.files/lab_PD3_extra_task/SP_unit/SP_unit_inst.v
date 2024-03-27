9174bcc73dc23029c344ab298c76a899f4bbd940
MatNepo <matnepo@gmail.com>
2024-03-27 19:47:17 +0300

    smth changed

---
+++ labs/lab10 - lab_PD3/.files/lab_PD3_extra_task/SP_unit/SP_unit_inst.v
@@ -0,0 +1,6 @@
+	SP_unit u0 (
+		.source     (<connected-to-source>),     //    sources.source
+		.probe      (<connected-to-probe>),      //     probes.probe
+		.source_clk (<connected-to-source_clk>)  // source_clk.clk
+	);
+
