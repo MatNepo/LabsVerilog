1497547586648871a0afec6a898ef37ebb2933e9
MatNepo <matnepo@gmail.com>
2024-04-13 13:05:28 +0300

    smth changed

---
+++ labs/lab12 - lab_NIOSII_1/lab_NIOSII_1/Lab1_task1/Lab1_task1.sv
@@ -0,0 +1,14 @@
+module Lab1_task1 (
+    input bit clk, // Clock
+    input bit sw,  // data in
+    input bit pbb, // Asynchronus reset active low
+    output bit [7:0] led
+);
+	Lab1_task1_nios u0 (
+		.clk_clk       (clk), //   clk.clk
+		.reset_reset_n (pbb), // reset.reset_n
+		.led_export    (led), //   led.export
+		.sw_export     (sw)   //    sw.export
+	);
+
+endmodule
---
+++ labs/lab12 - lab_NIOSII_1/lab_NIOSII_1/Lab1_task1/software/Lab1_task1_sw/obj/default/Lab1_task1_source.d
@@ -0,0 +1,1 @@
+obj/default/Lab1_task1_source.o: Lab1_task1_source.c
