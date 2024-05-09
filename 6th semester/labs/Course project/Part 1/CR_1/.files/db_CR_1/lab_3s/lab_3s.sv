7f2b4f4fbf73ca4ff7d56add6d1317909d0ce4bb
MatNepo <matnepo@gmail.com>
2024-05-09 14:50:05 +0300

    smth changed

---
+++ 6th semester/labs/Course project/Part 1/CR_1/.files/db_CR_1/lab_3s/lab_3s.sv
@@ -0,0 +1,28 @@
+//`include "lab_2s/ss_cntr.sv"
+//`include "b2bd_ROM.sv"
+module lab_3s (
+  input clk,
+  input  [7:0] bc,
+  output [6:0] ss,
+  output [4:1] dig
+);
+
+  wire [11:0] bdc;
+  ss_cntr #(1) UUT0 (
+    .clk  (clk),
+    .rst_n(1'b1),
+    .A    (4'd0),
+    .B    (bdc[11:8]),
+    .C    (bdc[7:4]),
+    .D    (bdc[3:0]),
+    .ss   (ss),
+    .dig  (dig)
+  );
+
+  b2bd_ROM UUT_ROM (
+    .clk(clk),
+    .bdc(bdc),
+    .bc (bc)
+  );
+
+endmodule
