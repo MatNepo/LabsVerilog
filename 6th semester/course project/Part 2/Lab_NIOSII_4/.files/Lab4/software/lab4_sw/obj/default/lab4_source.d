b4358081db84225c0ba7c929c46bdc009bea6dae
MatNepo <matnepo@gmail.com>
2024-05-11 20:58:12 +0300

    smth changed

---
+++ 6th semester/course project/Part 2/Lab_NIOSII_4/.files/Lab4/software/lab4_sw/obj/default/lab4_source.d
@@ -0,0 +1,25 @@
+obj/default/lab4_source.o: lab4_source.c ../lab4_sw_bsp/system.h \
+ ../lab4_sw_bsp/linker.h \
+ ../lab4_sw_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../lab4_sw_bsp//HAL/inc/io.h ../lab4_sw_bsp//HAL/inc/alt_types.h \
+ ../lab4_sw_bsp//HAL/inc/sys\alt_irq.h ../lab4_sw_bsp//HAL/inc/nios2.h \
+ ../lab4_sw_bsp//HAL/inc/alt_types.h \
+ ../lab4_sw_bsp//HAL/inc/sys\alt_stdio.h
+
+../lab4_sw_bsp/system.h:
+
+../lab4_sw_bsp/linker.h:
+
+../lab4_sw_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../lab4_sw_bsp//HAL/inc/io.h:
+
+../lab4_sw_bsp//HAL/inc/alt_types.h:
+
+../lab4_sw_bsp//HAL/inc/sys\alt_irq.h:
+
+../lab4_sw_bsp//HAL/inc/nios2.h:
+
+../lab4_sw_bsp//HAL/inc/alt_types.h:
+
+../lab4_sw_bsp//HAL/inc/sys\alt_stdio.h:
